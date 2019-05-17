7627e0fb5eb10930e166635ee0dbc78e8643d89b
Sain.EI <sain.ei@dns-shop.ru>
2019-05-17 18:43:00 +1000

    initial

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\api\ApiResponse;
+use Yii;
 use yii\rest\Controller;
 
 /**
@@ -10,20 +11,31 @@ use yii\rest\Controller;
  */
 abstract class ApiController extends Controller {
 
+	/** @var ApiResponse Ответ. */
 	protected $response;
 
 	/**
 	 * @inheritDoc
 	 */
 	public function beforeAction($action) {
+		Yii::$app->response->format = Yii::$app->response::FORMAT_JSON;
+
 		$this->response = new ApiResponse();
 
 		if (false === parent::beforeAction($action)) {
-			$this->response->code = ApiResponse::CODE_CATCHED;
-			$this->response->message = 'Нет доступа.';
+			$this->setResponse([], ApiResponse::CODE_CATCHED, 'Нет доступа.');
 
 			return false;
 		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function afterAction($action, $result) {
+		parent::afterAction($action, $result);
+
+		return $this->response;
 	}
 
 	/**
@@ -34,6 +46,8 @@ abstract class ApiController extends Controller {
 	 * @param string       $message Сообщение об ошибке.
 	 */
 	protected function setResponse($body, int $code = 0, string $message = '') {
-
+		$this->response->body    = $body;
+		$this->response->code    = $code;
+		$this->response->message = $message;
 	}
 }
